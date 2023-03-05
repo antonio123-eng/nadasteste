bafa6f9fc05b188086a4b4b44cd9c5ec4a4fca47
PatricioSouza <AntonioPatricio@gmail.com>
2023-03-05 10:06:23 -0300

    test212453

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ import express from 'express'
 import routes from './routes/Router.js'
 import {connectDB} from "./db/conn.js"
 import dotenv from 'dotenv'
+import cors from 'cors'
 dotenv.config()
 
 
 const app = express()
 connectDB()
+app.use(cors())
 const port = process.env.PORT || 5000
 app.use(express.json())
 
