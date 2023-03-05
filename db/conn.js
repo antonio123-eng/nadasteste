68a040f334731fa006a54407ae5de243963307f4
PatricioSouza <AntonioPatricio@gmail.com>
2023-03-05 09:54:55 -0300

    test23

--- db/conn.js
+++ db/conn.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose"
 
-export const connectDB = (url) => {
-   return mongoose.connect(url)
+export const connectDB = async() => {
+   await mongoose.connect("mongodb+srv://patricio:tJD97wEdwaOKbwUi@partydeploy.tjfhlm7.mongodb.net/?retryWrites=true&w=majority")
+   console.log("Conectou com sucesso")
 }
-
-
