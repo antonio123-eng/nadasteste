8addce05dcef1816ccc4c237c871927af254702e
PatricioSouza <AntonioPatricio@gmail.com>
2023-03-08 11:41:28 -0300

    upload de image12m

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import routes from './routes/Router.js'
 import {connectDB} from "./db/conn.js"
 import dotenv from 'dotenv'
+import cors from 'cors'
 dotenv.config()
 
 
@@ -9,6 +10,14 @@ const app = express()
 connectDB()
 const port = process.env.PORT || 5000
 app.use(express.json())
+
+app.use((req, res, next) => {
+   res.header("Access-Control-Allow-Origin", "*")
+   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE")
+   res.header("Access-Control-Allow-Headers", "Content-Type,Authorization")
+   app.use(cors())
+   next()
+})
 
 
 app.use("/api", routes)
