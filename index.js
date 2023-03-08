d497552da159c051cd820517b4451a426e28f52a
PatricioSouza <AntonioPatricio@gmail.com>
2023-03-08 11:56:44 -0300

    upload de sdsdm

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000
 app.use(express.json())
 
 app.use((req, res, next) => {
-   res.header("Access-Control-Allow-Origin", "*")
+   res.header("Access-Control-Allow-Origin", "https://nadasteste.onrender.com/")
    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE")
    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization")
    app.use(cors())
