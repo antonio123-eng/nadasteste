5283b1a4c691e2dc3b09259ef2fd0668c9c3405e
PatricioSouza <AntonioPatricio@gmail.com>
2023-03-10 11:51:20 -0300

    add routewew3223weewweews

--- index.js
+++ index.js
@@ -10,12 +10,14 @@ import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
+console.log(__dirname)
 
 const app = express()
 app.use(cors())
 app.use("/files", express.static(path.join(__dirname, "/uploads")))
 connectDB()
 const port = process.env.PORT || 5000
+app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
 app.use("/api", routes)
