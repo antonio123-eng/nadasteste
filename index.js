d93370d5cc3202a33435b7fb4d394f6f6952cea0
PatricioSouza <AntonioPatricio@gmail.com>
2023-03-10 15:13:12 -0300

    add rasweeweewsddsssweewewwes

--- index.js
+++ index.js
@@ -13,12 +13,15 @@ const __dirname = path.dirname(__filename)
 console.log(__dirname)
 
 const app = express()
+const port = process.env.PORT || 5000
+
 app.use(cors())
-app.use("/files", express.static(path.resolve(__dirname, "/uploads")))
+
+app.use("/files", express.static(path.join(__dirname, "/uploads")))
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
+
 connectDB()
-const port = process.env.PORT || 5000
 
 app.use("/api", routes)
 
