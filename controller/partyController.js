433b9672b95bb537beed6cf91da75b9150f6c2d4
PatricioSouza <AntonioPatricio@gmail.com>
2023-03-10 09:12:21 -0300

    add routes

--- controller/partyController.js
+++ controller/partyController.js
@@ -1,10 +1,8 @@
+import {Types} from "mongoose"
 import Party from "../models/Party.js"
 
 export const createParty = async(req, res) => {
 
-   //req.file.mimetype: image/png
-   // req.route.path = /parties
-   console.log(req.file)
    const { title, author, description, budget } = req.body
    let image
 
@@ -20,7 +18,6 @@ export const createParty = async(req, res) => {
       image
    }
 
-
    const response = await Party.create(party)
 
    res.status(201).json({response, msg: "Festa criada com sucesso."})
@@ -33,5 +30,92 @@ export const getAllParties = async(req, res) => {
    const parties = await Party.find().sort("-createdAt")
 
    res.json(parties)
+
+}
+
+
+export const getParty = async(req, res) => {
+
+   const { id } = req.params
+
+   if(!Types.ObjectId.isValid(id)) {
+      return res.status(422).json({msg: "Id com formato inválido"})
+   }
+
+   const party = await Party.findById(id)
+
+   if(!party) {
+      return res.status(404).json({msg: "Festa não encontrada."})
+   }
+
+   res.json(party)
+
+}
+
+
+export const deleteParty = async(req, res) => {
+
+   const { id } = req.params
+
+   if(!Types.ObjectId.isValid(id)) {
+      return res.status(422).json("Id com formato inválido")
+   }
+
+   try {
+      const party = await Party.findById(id)
+
+      if(!party) {
+         return res.status(404).json("Festa não encontrada.")
+      }
+
+      await Party.findByIdAndDelete(id)
+      return res.json({msg: "Festa removida com sucesso."})
+   } catch (error) {
+      res.status(500).json("Erro no servidor")
+   }
+
+}
+
+
+export const updateParty = async(req, res) => {
+
+   const { id } = req.params
+   const { title, author, description, budget } = req.body
+   const newParty = {}
+
+   if(!Types.ObjectId.isValid(id)) {
+      return res.status(422).json("Id com formato inválido")
+   }
+
+   if(title) {
+      newParty.title = title
+   }
+
+   if(author) {
+      newParty.author = author
+   }
+
+   if(description) {
+      newParty.description = description
+   }
+
+   if(budget) {
+      newParty.budget = budget
+   }
+
+   if(req.file) {
+      newParty.image = req.file.filename
+   }
+
+
+  try {
+      const party = await Party.findByIdAndUpdate(id, newParty)
+      if(!party) {
+         return res.status(404).json("Festa não encontrada.")
+      }
+      return res.json(party)
+  } catch (error) {
+      res.status(500).json("Erro no servidor")
+  }
 
 }
--- routes/partyRouter.js
+++ routes/partyRouter.js
@@ -1,10 +1,13 @@
 import express  from "express";
 const router = express.Router()
 
-import { createParty, getAllParties } from "../controller/partyController.js";
+import { createParty, deleteParty, getAllParties, getParty, updateParty } from "../controller/partyController.js";
 import upload from '../utils/UploadImage.js'
 
 router.post("/parties", upload.single("image"), createParty)
 router.get("/parties", getAllParties)
+router.get("/parties/:id", getParty)
+router.delete("/parties/:id", deleteParty)
+router.put("/parties/:id", updateParty)
 
 export default router
