e6a7fef60346c94832f5d6abf902ddf8f1501280
PatricioSouza <AntonioPatricio@gmail.com>
2023-03-10 14:44:29 -0300

    add rasweeweewsddssss

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ console.log(__dirname)
 
 const app = express()
 app.use(cors())
-app.use("/files", express.static(path.join(__dirname, "/uploads")))
+app.use("/files", express.static(path.resolve(__dirname, "/uploads")))
 connectDB()
 const port = process.env.PORT || 5000
 app.use(express.urlencoded({extended: false}))
