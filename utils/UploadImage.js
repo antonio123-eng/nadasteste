f5ed0cc29f68116f0e16ca0f38129015ce1557aa
PatricioSouza <AntonioPatricio@gmail.com>
2023-03-13 09:37:51 -0300

    add rasweqwqwdsdsyuue

--- utils/UploadImage.js
+++ utils/UploadImage.js
@@ -1,5 +1,9 @@
 import multer from 'multer'
 import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const storage = multer.diskStorage({
    destination: (req, file, cb) => {
@@ -9,7 +13,7 @@ const storage = multer.diskStorage({
       } else if(req.url.includes("parties")) {
          folder = "parties"
       }
-      cb(null, `uploads/${folder}`)
+      cb(null, path.join(__dirname, `../uploads/${folder}`))
    },
    filename: (req, file, cb) => {
       cb(null, Date.now() + String(Math.floor(Math.random() * 10000)) + path.extname(file.originalname))
